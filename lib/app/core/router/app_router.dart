bbfbc7136256d76398fb2e130494f7aa55905746
Hager Darwish <107562928+Hagerdarwish@users.noreply.github.com>
2025-12-19 22:08:08 +0200

    setup app routing

---
+++ lib/app/core/router/app_router.dart
@@ -0,0 +1,14 @@
+import 'package:go_router/go_router.dart';
+
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/', //  start here
+  routes: [
+
+   // GoRoute(
+   //   path: '/',
+   //   name: 'login',
+   //   builder: (context, state) => const LoginPage(),
+   // ),
+  ],
+);
