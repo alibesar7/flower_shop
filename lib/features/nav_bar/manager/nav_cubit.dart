d9f27ace59ba5acad9716fba3cd81ea12c2f23d9
Rahma Ashraf <rahmaashraf.dev@gmail.com>
2025-12-23 19:22:24 +0200

    modified navcubit

--- lib/features/nav_bar/manager/nav_cubit.dart
+++ lib/features/nav_bar/manager/nav_cubit.dart
@@ -1,17 +1,11 @@
 import 'package:bloc/bloc.dart';
-import 'nav_state.dart';
+import 'package:flower_shop/features/nav_bar/manager/nav_state.dart';
+
 
 class NavCubit extends Cubit<NavState> {
   NavCubit() : super(const NavInitial(selectedIndex: 0));
 
   void updateIndex(int index) {
-    // Emit a new instance without creating separate class
-    emit(_NavUpdated(selectedIndex: index));
+    emit(NavState(selectedIndex: index));
   }
-}
-
-// Private class for updated state
-class _NavUpdated extends NavState {
-  const _NavUpdated({required int selectedIndex}) 
-      : super(selectedIndex: selectedIndex);
 }
